98d868472b5c78425c7b2080deabb3fde97a6dda
Stat Plans <tecnico@statplans.es>
2022-02-12 13:36:02 +0100

    First commit with initial structure and configuration for a maven project

---
+++ src/main/java/com/sanitas/calculator/CalculatorApplication.java
@@ -0,0 +1,17 @@
+package com.sanitas.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author imoreno
+ * @since 2022-02-12
+ */
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.sanitas.calculator.CalculatorApplication.class, args);
+	}
+
+}
